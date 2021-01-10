bedb613741866e9c278215584e5d91446baab5b2
Randy Coulman <rcoulman@influxdata.com>
2021-01-09 17:01:33 -0800

    📌 Allow earlier Elixir versions (>= 1.8)

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule MixTestInteractive.MixProject do
     [
       app: :mix_test_interactive,
       version: "0.1.0",
-      elixir: "~> 1.11",
+      elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
