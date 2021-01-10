c3400215503d1633f638939eb45a602c38350697
Randy Coulman <rcoulman@influxdata.com>
2021-01-09 18:23:10 -0800

    ♻️ Extract version to module attribute

--- mix.exs
+++ mix.exs
@@ -1,10 +1,12 @@
 defmodule MixTestInteractive.MixProject do
   use Mix.Project
 
+  @version "0.1.0"
+
   def project do
     [
       app: :mix_test_interactive,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
       deps: deps()
