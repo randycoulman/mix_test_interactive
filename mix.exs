6432a96a8f067c36bec402498531ed7082eb58c8
Randy Coulman <rcoulman@gmail.com>
2023-09-15 17:35:29 -0700

    ⬆️ Upgrade to latest ex_doc

--- mix.exs
+++ mix.exs
@@ -32,7 +32,7 @@ defmodule MixTestInteractive.MixProject do
 
   defp deps do
     [
-      {:ex_doc, "~> 0.28.3", only: :dev, runtime: false},
+      {:ex_doc, "~> 0.30.6", only: :dev, runtime: false},
       {:file_system, "~> 0.2"},
       {:styler, "~> 0.9.3"},
       {:temporary_env, "~> 2.0", only: :test},
