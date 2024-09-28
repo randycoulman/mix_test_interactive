f159f4f314b52dc237e078ef912d1152d8064863
Randy Coulman <randy@randycoulman.com>
2024-09-28 11:56:53 -0700

    🏷️ Prepare v4.1.1 release (#124)

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule MixTestInteractive.MixProject do
   use Mix.Project
 
-  @version "4.1.0"
+  @version "4.1.1"
   @source_url "https://github.com/randycoulman/mix_test_interactive"
 
   def project do
